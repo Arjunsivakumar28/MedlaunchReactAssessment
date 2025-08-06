31d155c1b6282446790ac5f9ce3e7195be1973b8
Arjun <arjuns8@illinois.edu>
2025-08-06 12:29:40 -0400

    minor formatting change for vercel deployment

--- src/App/Store.jsx
+++ src/App/Store.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import FormReducer from '../Features/Form/formSlice'
+import FormReducer from '../Features/Form/FormSlice'
 
 
 export default configureStore({
